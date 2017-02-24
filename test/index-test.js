049c89dead68ec0157273dbef400113b26d0a599
Marissa Reinke <reinke.marissa@gmail.com>
2017-02-23 18:03:18 -0700

    setup 7 hour forecast

--- test/index-test.js
+++ test/index-test.js
@@ -6,6 +6,8 @@ import CurrentWeather from '../lib/components/CurrentWeather';
 import Daily from '../lib/components/Daily';
 import Hourly from '../lib/components/Hourly';
 import { shallow, mount, render } from 'enzyme';
+const forecast10 = require('./forecast10.json');
+
 
 describe('Main', () => {
   it('should have a component called Weather', () => {
@@ -29,4 +31,14 @@ describe('Weather', () => {
     searchInput.simulate('change', { target: { value: 'charlieville' } });
     expect(wrapper.state().location).to.equal('charlieville');
   });
+  // it('should take location and submit it and get an object', () => {
+  //   const wrapper = shallow(<Weather/>);
+  //   const searchInput = wrapper.find('.search');
+  //   const submit = wrapper.find('.go');
+  //   expect(wrapper.state().location).to.equal('');
+  //   searchInput.simulate('change', { target: { value: 'Denver, CO' } });
+  //   expect(wrapper.state().location).to.equal('Denver, CO');
+  //   submit.simulate('click');
+  //   expect(wrapper.state().weather.length).to.equal(1);
+  // });
 });
